3d1801e2b04c3de9977f23c538e727dc61bf920c
elinsky <dmitry.ilinsky@gmail.com>
2024-06-29 14:08:22 +0300

    something going on. colors looks ok and sides close to done

--- module6hard.py
+++ module6hard.py
@@ -18,25 +18,18 @@ class Figure:
         if self._is_valid_color(color):
             self._color = color
 
-
-    def _is_valid_sides(self):
-        if len(self._sides) != self.sides_count:
-            return False
-        for side in self._sides:
-            if side <= 0:
-                return False
-        return True
-
-    def get_sides(self):
-        return self._sides
+    def _is_valid_sides(self, new_sides):
+        if isinstance(new_sides, tuple) and len(new_sides) == 1:
+            return True
 
     def set_sides(self, *sides):
-        if self._is_valid_sides():
+        if self._is_valid_sides(sides):
             self._sides = sides
         else:
             self._sides = [sides[0]] * self.sides_count
 
-    #        print(self._sides)
+    def get_sides(self):
+        return self._sides
 
     def __len(self):
         return sum(self._sides)
@@ -77,27 +70,33 @@ class Cube(Figure):
         self.set_color(color[0], color[1], color[2])
         self.set_sides(edge)
 
+    def set_sides(self, *sides):
+        if len(sides) == 1:
+            if self._is_valid_sides(sides):
+                self._sides = [sides[0]] * self.sides_count
+
+
     def get_volume(self):
         return self._sides[0] ** 3
 
 
-circle1 = Circle((200, 200, 100), 10)  # (Цвет, стороны)
-cube1 = Cube((222, 35, 130), 12)
+circle1 = Circle((200, 200, 100), 10) # (Цвет, стороны)
+cube1 = Cube((222, 35, 130), 6)
 
 # Проверка на изменение цветов:
-circle1.set_color(55, 66, 77)  # Изменится
-cube1.set_color(300, 70, 15)  # Не изменится
+circle1.set_color(55, 66, 77) # Изменится
+cube1.set_color(300, 70, 15) # Не изменится
 print(circle1.get_color())
 print(cube1.get_color())
-#
-## Проверка на изменение сторон:
-#cube1.set_sides(5, 3, 12, 4, 5)  # Не изменится
-#circle1.set_sides(15)  # Изменится
-#print(cube1.get_sides())
-#print(circle1.get_sides())
-#
-## Проверка периметра (круга), это и есть длина:
-#print(len(circle1))
-#
-## Проверка объёма (куба):
-#print(cube1.get_volume())
+
+# Проверка на изменение сторон:
+cube1.set_sides(5, 3, 12, 4, 5) # Не изменится
+circle1.set_sides(15) # Изменится
+print(cube1.get_sides())
+print(circle1.get_sides())
+
+# Проверка периметра (круга), это и есть длина:
+print(len(circle1))
+
+# Проверка объёма (куба):
+print(cube1.get_volume())
